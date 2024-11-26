8c4b01ffe600b6b51e3455aa0f51f9ca8aded9ab
kstubhie <banekaustubh27@gmail.com>
2024-11-27 00:57:19 +0530

    ui

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -35,7 +35,7 @@ const TaskList = () => {
                 <h2 className='mt-5 text-xl font-semibold'>Make a youtube video</h2>
                 <p className='mt-2 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, corrupti quisquam dolorem sint commodi veritatis.</p>
             </div>
-            <div className='h-full flex-shrink-0 rounded-2xl p-5 w-[300px] bg-yellow-300'>
+            <div className='h-full flex-shrink-0 rounded-2xl p-5 w-[300px] bg-yellow-400'>
                 <div className='flex justify-between items-center'>
                     <h3 className='px-3 py-1 bg-red-500 rounded-lg'>High</h3>
                     <h4>24 Nov 2024</h4>
