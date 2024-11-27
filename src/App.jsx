03a488f829a1a5d79f1fdffcdb5e6e60802c9c2d
kstubhie <banekaustubh27@gmail.com>
2024-11-28 00:05:06 +0530

    role based auth using json

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 /* eslint-disable no-unused-vars */
 import './App.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
-
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
   const [user, setUser] = useState("")
 
+  const authData = useContext(AuthContext)
+  // console.log(authData);
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("loggedInUser")
+      if (loggedInUser) {
+        setUser(loggedInUser.role)
+      }
+    }
+  }, [authData])
+
   const handleLogin = (email, password) => {
-    if (email == "admin@me.com" && password == '123') {
+    if (authData && authData.admin.find((e) => email == e.email && password == e.password)) {
       setUser("admin");
       // console.log(user);
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: "admin" }))
 
-    } else if (email == "user@me.com" && password == '123') {
+    }
+    else if (authData && authData.employees.find((e) => email == e.email && password == e.password)) {
       setUser("employee");
       // console.log(user);
-
-    } else {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: "employee" }))
+    }
+    else {
       alert("invalid")
     }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/AuthContext.jsx'
-import TaskContext from './context/TaskContext.jsx'
-
+import AuthProvider from './context/AuthProvider.jsx'
 // localStorage.clear();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContext> {/* kuch bhi pehle likh skte hai like authcontext bhi ya taskcontext bhi*/}
-      <TaskContext> {/* kuch bhi pehle likh skte hai like authcontext bhi ya taskcontext bhi*/}
-        <App />
-      </TaskContext>
-    </AuthContext>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
